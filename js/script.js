2627129608e319435cdbd117bd451d25984c060d
ben.meek <ben.meek@aardman.com>
2017-09-12 11:24:27 +0100

    image carousel

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
 $(function(){
-	var dir = "../img/";
-	var fileextension = ".jpg";
-	$.ajax({
-		url: dir,
-		success: function (data) {
-			$(data).find("a:contains(" + fileextension + ")").each(function () {
-				var filename = this.href.replace(window.location.host, "").replace("http://", "");
-				$(".owl-carousel").append("<img src='" + dir + filename + "'>");
-			});
-		}
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		center: true,
+		lazyLoad: true
 	});
-	$(".owl-carousel").owlCarousel();
 });

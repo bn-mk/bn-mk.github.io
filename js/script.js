25c57036efccfd7717053786a86b930f91434aea
ben.meek <ben.meek@aardman.com>
2017-09-12 12:12:20 +0100

    isotope layout

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 $(function(){
-	$(".owl-carousel").owlCarousel({
-		items: 1,
-		loop: true,
-		center: true,
-		lazyLoad: true,
-		dots: false
+	// $(".owl-carousel").owlCarousel({
+	// 	items: 1,
+	// 	loop: true,
+	// 	center: true,
+	// 	lazyLoad: true,
+	// 	dots: false
+	// });
+
+	$('.grid').isotope({
+		itemSelector: '.grid-item',
+		percentPosition: true,
+		masonry: {
+			columnWidth: '.grid-sizer'
+		}
+	});
+	$grid.imagesLoaded().progress( function() {
+		$grid.isotope('layout');
 	});
 });
